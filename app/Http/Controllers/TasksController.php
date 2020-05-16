b32701a412bdf2a46a59914409a88009f29cfdce
yushi.kasai <abcd33385@gmail.com>
2020-05-16 15:00:09 +0000

    add column

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,129 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view("tasks.index",[
+            "tasks" => $tasks,
+            ]);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $task = new Task;
+        
+        return view("tasks.create",[
+            "task"=>$task,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            "status"=>"required|max:191",
+            "content"=>"required|max:191",
+            ]);
+        
+        
+        $task =new Task;
+        $task ->status = $request->status;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect("/");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task=Task::find($id);
+        
+        return view("tasks.show",[
+            "task"=> $task,
+            ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        
+        return view("tasks.edit",[
+            "task" => $task,
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+            "status"=>"reqired|max:191",
+            "content"=>"required|max:191",
+            ]);
+            
+        $task =Task::find($id);
+        $task->status=$request->status;
+        $task->content=$request->content;
+        $task->save();
+        
+        return redirect("/");
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect("/");
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Tasklist</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    </head>
+    <body>
+
+        @include('commons.navbar')
+        
+        <div class="container">
+            @yield('content')
+        </div>
+        
+        <!-- JavaScriptの指定は省略 -->
+        
+    </body>
+</html>
